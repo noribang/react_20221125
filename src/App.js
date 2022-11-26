4740952dd5640e8f3b1f4025e831fcc1237367fa
hoonkim <hoonk9387@gmail.com>
2022-11-26 16:17:18 -0600

    cleaned up comments from app.js.

--- src/App.js
+++ src/App.js
@@ -3,41 +3,8 @@
 import './style.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import EmojiButton from './EmojiButton';
 import Header from './Header';
 import HomePage from './HomePage';
-
-// // Composition-children
-// function Card(props) {
-//   console.log(props)
-//   return (
-//     <div className="card">
-//       <h2>{props.title}</h2>
-//       {props.children}
-//     </div>
-//   )
-// }
-
-// // HomePage component.
-// function HomePage() {
-//   return (
-//     <main>
-//       <Card title="Welcome to PetLaaaand!">
-//         <em>Find your dream pet</em>
-//         <p>Like puppies....</p>
-//         <h3>Like kittens...</h3>
-//       </Card>
-//       <Card title="What pets would you like to see?">
-//         <div>
-//             <EmojiButton emoji="😸" labelText="Cats"/>
-//             <EmojiButton emoji="🐕" labelText="Dogs" />
-//         </div>
-//       </Card>
-//     </main>
-//   )
-// }
-
-
 
 function App() {
   return (
