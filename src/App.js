bdd85f7122d323cf59889f87f4732df9feb65f5c
hoonkim <hoonk9387@gmail.com>
2022-11-25 21:36:22 -0600

    created component inside app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// function createCard(greeting, subheader) {
+//   return (
+//     <div>
+//       <h1>{greeting}</h1>
+//       <h2>{subheader}</h2>
+//     </div>
+//   )
+// }
+
+function Card(props) {
+  return (
+    <div>
+      <h1>{props.greeting}</h1>
+      <h2>{props.subheader}</h2>
+    </div>
+  )
+}
+
+const element = (
+  <div>
+    <Card greeting="Hola!" subheader="Friday!"/>
+    <Card greeting="Greetings!" subheader="Humans!"/>
+    {/* {Card({greeting: "Hola from JSX!", subheader:"Time to learn"})}
+    {Card({greeting: "Ques component?", subheader: "Let's find out!"})} */}
+  </div>
+)
+
 
 function App() {
   return (
     <div className="App">
       <h1>Cleaned up...</h1>
+      {element}
     </div>
   );
 }
