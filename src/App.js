a91f2de2454ef705548ec4930297e9ef37cf60a7
hoonkim <hoonk9387@gmail.com>
2022-11-26 14:20:13 -0600

    added header and homepage components.

--- src/App.js
+++ src/App.js
@@ -30,12 +30,64 @@ const element = (
   </div>
 )
 
+// Button component.
+function EmojiButton(props) {
+  console.log(props)
+  return (
+    <button>
+      <span role="img">{props.emoji}</span>
+      {props.labelText}
+    </button>
+  )
+}
+// Header component
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.heading}</h1>
+      <nav>
+        <EmojiButton emoji="👤" labelText="Login"/>
+      </nav>
+    </header>
+  )
+} 
+// HomePage component.
+function HomePage() {
+  return (
+    <main>
+      <div className="card">
+        <h2>Welcome to PetLand!</h2>
+        <em>Find your dream pet</em>
+      </div>
+      <div className="card">
+        <h2>What pets would you like to see?</h2>
+        <div>
+          <EmojiButton emoji="😸" labelText="Caaats"/>
+          <EmojiButton emoji="🐕" labelText="Dogs" />
+        </div>
+      </div>
+    </main>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Cleaned up...</h1>
-      {element}
+    <div>
+      <Header heading="PetLand" />
+      <HomePage />
+      {/* <main>
+        <div className="card">
+          <h2>Welcome to PetLand!</h2>
+          <em>Find your dream pet</em>
+        </div>
+        <div className="card">
+          <h2>What pets would you like to see?</h2>
+          <div>
+            <EmojiButton emoji="😸" labelText="Caaats"/>
+            <EmojiButton emoji="🐕" labelText="Dogs" />
+          </div>
+        </div>
+      </main> */}
     </div>
   );
 }
